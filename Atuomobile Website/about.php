d2b7f8db0e8f64b58aa989d22566a1308f415808
Jameel Ahmed. J <123755027+Jameel-Ahmed-2003@users.noreply.github.com>
2023-05-30 19:00:03 +0530

    Add files via upload

---
+++ Atuomobile Website/about.php
@@ -0,0 +1,53 @@
+<?php include("header.html")?>
+<body>
+<div class="about">
+
+<div class="heading">
+<h1>about us</h1>
+</div>
+
+<div class="content">
+<p><center>Museum</center></p>
+</div>
+<div class="img3">
+<div class="i1">
+<marquee>
+<img src="images/Sharafath Bilali 20160513_102822.jpg"height="350px" width="250px"/>
+</marquee>
+</div>
+<div
+class="i2">
+<marquee>
+<img src="images/96db7360427441.5a4cdc300f0e7.jpg"height="350px" width="250px" /></marquee></div>
+<div class="i3">
+<marquee>
+<img src="images/2013_lamborghini_aventador_lp700_4-wide - Copy.jpg"height="350px" width="250px"/></marquee></div>
+<div class="i4">
+<marquee>
+<img src="images/1552642371185.jpg"height="350px" width="250px"/>
+</marquee>
+</div>
+</div>
+<div class="img4">
+<div class="i5">
+<marquee>
+<img src="images/car-1554994675298-5751.jpg" height="350px" width="250px"/>
+</marquee>
+</div>
+<div class="i6">
+<marquee>
+<img src="images/BestWallpapers-1543741022896.jpg" height="350px" width="250px" />
+</marquee>
+</div>
+<div class="i7">
+<marquee>
+<img src="images/BestWallpapers-1543936757031.jpg"height="350px" width="250px"/>
+</marquee>
+</div>
+<div class="i8">
+<img src="images/BestWallpapers-1543741004141.jpg"height="350px" width="250px"/>
+</div>
+</div>
+</div>
+</body>
+<?php include("footer.html")?>
